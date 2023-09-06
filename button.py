68afebea9d6c82148108e3b437eea912419abfcc
wwright71 <jwright71@charlottesvilleschools.org>
2023-09-06 11:00:05 -0400

    Added 2nd button variable and functions

--- button.py
+++ button.py
@@ -6,11 +6,23 @@ btn1 = DigitalInOut(board.D2)
 btn1.direction = Direction.INPUT
 btn1.pull = Pull.DOWN
 
+btn2 = DigitalInOut(board.D3)
+btn2.direction = Direction.INPUT
+btn2.pull = Pull.DOWN
+
 while True:
     if  btn1.value:
         print("BTN1 is down")
     else:
         print("BTN1 is up")
+        pass
+
+    time.sleep(0.1) # sleep for debounce
+
+    if  btn2.value:
+        print("BTN2 is down")
+    else:
+        print("BTN2 is up")
         pass
 
     time.sleep(0.1) # sleep for debounce
